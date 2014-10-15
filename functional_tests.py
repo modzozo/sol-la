3f99ba717434181cf390a5454bc2b4e99751b906
modzozo <modzozo@abv.bg>
2014-10-15 17:09:45 +0300

    fix unittest TypeError, add proper response

--- functional_tests.py
+++ functional_tests.py
@@ -7,8 +7,9 @@ class NewVisitorTest(unittest.TestCase):  #1
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
-    def tearDown(self):  #3
-       # self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):  #4
         # Edith has heard about a cool new online to-do app. She goes
@@ -16,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):  #1
         self.browser.get('http://localhost:8000')
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)  #5
+        self.assertIn('To-Do lists', self.browser.title)  #5
         self.fail('Finish the test!')  #6
 
         # She is invited to enter a to-do item straight away
